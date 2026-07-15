74a3ef386e672ae94df9b199927d41a921d44d15
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-07-15 23:56:27 +0900

    Add files via upload

---
+++ 100.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+char Hello[] = "Hello, World!!\n";
+char kekka[15000300] = "";
+char *ptr = kekka; // ★次に書き込むメモリの場所を指すポインタ
+void p() {
+    ptr+=15;
+}
+void a() {
+    memcpy(ptr,Hello,15);p();
+    memcpy(ptr,Hello,15);p();
+    memcpy(ptr,Hello,15);p();
+    memcpy(ptr,Hello,15);p();
+    memcpy(ptr,Hello,15);p();
+    memcpy(ptr,Hello,15);p();
+    memcpy(ptr,Hello,15);p();
+    memcpy(ptr,Hello,15);p();
+    memcpy(ptr,Hello,15);p();
+    memcpy(ptr,Hello,15);p();
+}
+void b() {
+    a();a();a();a();a();a();a();a();a();a();a();a();a();a();a();a();a();a();a();a();
+}
+void c() {
+    b();b();b();b();b();b();b();b();b();b();b();b();b();b();b();b();b();b();b();b();b();b();b();b();b();
+}
+void d() {
+    c();c();c();c();c();c();c();c();c();c();
+}
+int main() {
+    d();d();d();d();d();d();d();d();d();d();d();d();d();d();d();d();d();d();d();d();
+    printf("%s",kekka);
+}
