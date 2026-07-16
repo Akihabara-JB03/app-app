32e8d84bdb79b80f15994acc32c53fd643f05141
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-07-16 15:37:36 +0900

    Update main.nim

--- main.nim
+++ main.nim
@@ -3,4 +3,12 @@ proc m() {.importc: "m",cdecl.}
 
 m()
 echo "---100万回のHello, World!!に耐えられましたか？---"
-echo 
+echo "これだけでパソコンが重くなるならもう買い替えろ案件！！"
+echo "重くなりましたか？ (y/n)"
+var nyu  = readLine(stdin)
+if nyu == "y":
+  echo "パソコン買い換えたほうがいいんじゃないですか？"
+elif nyu == "n":
+  echo "パソコンはまあ別の診断を受けてそこで重かったら～みたいな感じにすればいいんじゃないですか？"
+else:
+  echo "応対すらままらないような奴がこんな診断に来るな！！"
