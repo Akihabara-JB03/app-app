2e04dac926723daa6a70fca84f02c4f6d4b5f1f1
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-07-16 16:09:29 +0900

    Update sub.nim

--- sub.nim
+++ sub.nim
@@ -1,13 +1,15 @@
 proc m() {.importc: "m", cdecl.}
-
+import std/os # ★時間を止めるためのライブラリを読み込む
+proc s() =
+  sleep(125)
 proc m10*() =
-  m()
-  m()
-  m()
-  m()
-  m()
-  m()
-  m()
-  m()
-  m()
-  m()
+  m();s()
+  m();s()
+  m();s()
+  m();s()
+  m();s()
+  m();s()
+  m();s()
+  m();s()
+  m();s()
+  m();s()
